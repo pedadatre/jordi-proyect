b3be25a14f0d0cc815a6c678dc3f54475563cce2
pedadatre <pedadatre@alu.edu.gva.es>
2024-12-09 12:32:53 +0100

    Update redirect routes from 'dashboard' to 'home' in authentication controllers and add remember_token to users table

---
+++ database/migrations/2024_12_09_100257_add_remember_token_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->rememberToken()->after('password'); // Agregar la columna remember_token después de la columna password
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            if (Schema::hasColumn('users', 'remember_token')) {
+                $table->dropColumn('remember_token'); // Eliminar la columna remember_token
+            }
+        });
+    }
+};
